81b3c79021a0267696eff0ecc4785078e85bfce8
humbertdz <l21te0133@teziutlan.tecnm.mx>
2025-11-11 08:00:13 -0600

    componente tradicional

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <h1>Mi primera app</h1>
   <hr />
   <!-- y se renderiza el componente -->
-  <MyCounter :counter-init="3" />
+  <!-- <MyCounter :counter-init="3" /> -->
+   <MyCounter2 :counterInit="3" />
 </template>
 
 <script setup lang="ts">
 // se importa en el script
 import MyCounter from './components/MyCounter.vue';
+import MyCounter2 from './components/MyCounter2.vue';
 </script>
---
+++ src/components/MyCounter2.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>Counter: {{ counter }}</h1>
+  <h1>Counter elevado computado: {{ counterElevatedComputed }}</h1>
+
+  <!-- cuando se accede a valores desde el html vue extrae directamente el value -->
+  <button @click="counter++">+</button>
+  <button @click="counter--">-</button>
+</template>
+
+<!-- aplicando el setup directamente desde el script -->
+<script lang="ts">
+import { computed, defineComponent, ref } from 'vue';
+// se exporta por default la definicion de componente
+export default defineComponent({
+  props: {
+    counterInit: { type: Number, required: true },
+  },
+
+  setup(props) {
+    const counter = ref<number>(props.counterInit);
+    const counterElevatedComputed = computed(() => counter.value * counter.value);
+
+    return {
+      counter,
+      counterElevatedComputed,
+    };
+  },
+});
+</script>
+
+<!-- con style solo. se aplican los estilos a todo el dom. -->
+<!-- con scoped aplica los estilos a solo este SFC SINGLE FILE COMPONENT y agrega un data-v para manejar un correcto scoped -->
+<style scoped>
+h1 {
+  text-shadow: 1px 1px red;
+}
+</style>
