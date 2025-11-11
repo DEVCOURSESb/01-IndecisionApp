c48fef9515da40ab1454e05a3348a14d97c0c955
humbert dz <l21te0133@teziutlan.tecnm.mx>
2025-11-10 19:15:37 -0600

    primer componente

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <template>
-  <h1>Counter: {{ counter }}</h1>
-  <h1>Counter elevado computado: {{ counterElevatedComputed }}</h1>
-
-  <!-- cuando se accede a valores desde el html vue extrae directamente el value -->
-  <button @click="counter++">+</button>
-  <button @click="counter--">-</button>
+  <h1>Mi primera app</h1>
+  <hr />
+  <!-- y se renderiza el componente -->
+  <MyCounter />
 </template>
 
-<!-- aplicando el setup directamente desde el script -->
-<script lang="ts" setup>
-import { computed, ref } from 'vue';
-
-const counter = ref<number>(10);
-const counterElevatedComputed = computed(() => counter.value * counter.value);
+<script setup lang="ts">
+// se importa en el script
+import MyCounter from './components/MyCounter.vue';
 </script>
-
-<!-- con style solo. se aplican los estilos a todo el dom. -->
-<!-- con scoped aplica los estilos a solo este SFC SINGLE FILE COMPONENT y agrega un data-v para manejar un correcto scoped -->
-<style scoped>
-h1 {
-  text-shadow: 1px 1px red;
-}
-</style>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Counter: {{ counter }}</h1>
+  <h1>Counter elevado computado: {{ counterElevatedComputed }}</h1>
+
+  <!-- cuando se accede a valores desde el html vue extrae directamente el value -->
+  <button @click="counter++">+</button>
+  <button @click="counter--">-</button>
+</template>
+
+<!-- aplicando el setup directamente desde el script -->
+<script lang="ts" setup>
+import { computed, ref } from 'vue';
+
+const counter = ref<number>(10);
+const counterElevatedComputed = computed(() => counter.value * counter.value);
+</script>
+
+<!-- con style solo. se aplican los estilos a todo el dom. -->
+<!-- con scoped aplica los estilos a solo este SFC SINGLE FILE COMPONENT y agrega un data-v para manejar un correcto scoped -->
+<style scoped>
+h1 {
+  text-shadow: 1px 1px red;
+}
+</style>
