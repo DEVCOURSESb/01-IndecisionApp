f5375ca807c5389de21f6e8e0cba56b53ddcf8f4
humbertdz <l21te0133@teziutlan.tecnm.mx>
2025-11-11 12:23:17 -0600

    continuacion de la seccion

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
   <h1>Mi primera app</h1>
   <hr />
-  <!-- y se renderiza el componente -->
-  <MyCounter />
-  <MyCounter2 :counterInit="10" />
 </template>
 
 <script setup lang="ts">
-// se importa en el script
-import MyCounter from './components/MyCounter.vue';
-import MyCounter2 from './components/MyCounter2/MyCounter2.vue';
 </script>
