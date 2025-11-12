2304e6f258de2b3fad84cf0d8579e96a7a55f242
humbertdz <l21te0133@teziutlan.tecnm.mx>
2025-11-12 08:13:35 -0600

    pensemos en composables

--- src/composableFunctions/useMessages.ts
+++ src/composableFunctions/useMessages.ts
@@ -1,9 +1,32 @@
+import type { ChatMessage } from '@/interfaces/ChatMessage';
+import { ref } from 'vue';
+
 export const useMessages = () => {
   const createUUID = () => {
     return new Date().getTime();
   };
 
+  const messages = ref<ChatMessage[]>([
+    { id: createUUID(), itsMine: true, message: 'Hola' },
+    { id: createUUID(), itsMine: true, message: 'Hola x2' },
+    { id: createUUID(), itsMine: false, message: 'Hola respuesta', image: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png', },
+  ]);
+
+  /**
+   * declaracion de función para agregar un mensaje a la lista reactiva
+   * @param message a agregar
+   */
+  const onSendMessage = (message: string) => {
+    messages.value.push({
+      id: createUUID(),
+      itsMine: true,
+      message,
+    });
+  };
+
   return {
     createUUID,
+    messages,
+    onSendMessage
   };
 };
--- src/views/IndesitionView.vue
+++ src/views/IndesitionView.vue
@@ -7,7 +7,7 @@
     <ChatMessages :messages />
 
     <!-- es indistinto si declaramos una prop con camelCase y la enviamos en kebab-case -->
-    <MessageBox @on-send-message="onSendMessageFather" />
+    <MessageBox @on-send-message="onSendMessage" />
   </div>
 </template>
 
@@ -15,27 +15,7 @@
 import ChatMessages from '@/components/chat/ChatMessages.vue';
 import MessageBox from '@/components/chat/MessageBox.vue';
 import { useMessages } from '@/composableFunctions/useMessages';
-import type { ChatMessage } from '@/interfaces/ChatMessage';
-import { ref } from 'vue';
 
+const { messages, onSendMessage } = useMessages();
 
-const { createUUID } = useMessages();
-
-const messages = ref<ChatMessage[]>([
-  { id: createUUID(), itsMine: true, message: 'Hola' },
-  { id: createUUID(), itsMine: true, message: 'Hola x2' },
-  { id: createUUID(), itsMine: false, message: 'Hola respuesta', image: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',},
-]);
-
-/**
- * declaracion de función para agregar un mensaje a la lista reactiva
- * @param message a agregar
- */
-const onSendMessageFather = (message: string) => {
-  messages.value.push({
-    id: createUUID(),
-    itsMine: true,
-    message,
-  });
-};
 </script>
