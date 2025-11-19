62fa059fa01694e4e51e50f8eee1753c83b6babd
humbertdz <l21te0133@teziutlan.tecnm.mx>
2025-11-19 11:29:04 -0600

    reallizar peticion HTTP

--- src/composableFunctions/useMessages.ts
+++ src/composableFunctions/useMessages.ts
@@ -1,4 +1,5 @@
 import type { ChatMessage } from '@/interfaces/ChatMessage';
+import type { YesNoResponse } from '@/interfaces/YesNoResponse';
 import { ref } from 'vue';
 
 export const useMessages = () => {
@@ -12,17 +13,31 @@ export const useMessages = () => {
     { id: createUUID(), itsMine: false, message: 'Hola respuesta', image: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png', },
   ]);
 
+  const getHerResponse = async () => {
+    const response = await fetch("https://yesno.wtf/api");
+    // como no se puede tipar el resultado, se indica que trate como
+    const data = (await response.json()) as YesNoResponse;
+    return data;
+  }
+
   /**
    * declaracion de función para agregar un mensaje a la lista reactiva
    * @param message a agregar
    */
-  const onSendMessage = (message: string) => {
+  const onSendMessage = async (message: string) => {
     messages.value.push({
       id: createUUID(),
       itsMine: true,
       message,
     });
+
+    if(!message.endsWith('?')) return;
+
+    const { answer, image } = await getHerResponse();
+
+    messages.value.push({ id: createUUID(), itsMine: false, message: answer, image });
   };
+
 
   return {
     createUUID,
